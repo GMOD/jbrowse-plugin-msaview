2e885a1cf1d4233d2aec375888126afb0ed98fc3
Colin Diesh <colin.diesh@gmail.com>
2021-03-17 18:41:00 -0400

    Depend on react-msaview (#23)
    * Remove many files
    
    * Use proper last working version
    
    * Remove cypress mentions
    
    * Remove some console logs

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
-import Plugin from "@jbrowse/core/Plugin";
-import PluginManager from "@jbrowse/core/PluginManager";
-import { version } from "../package.json";
-import {
-  AbstractSessionModel,
-  isAbstractMenuManager,
-} from "@jbrowse/core/util";
-import GridOn from "@material-ui/icons/GridOn";
-import MsaViewFactory from "./MsaView";
+import Plugin from '@jbrowse/core/Plugin'
+import PluginManager from '@jbrowse/core/PluginManager'
+import { version } from '../package.json'
+import { AbstractSessionModel, isAbstractMenuManager } from '@jbrowse/core/util'
+import { MSAModel, MSAView } from 'react-msaview'
+import ViewType from '@jbrowse/core/pluggableElementTypes/ViewType'
 
 export default class MsaViewPlugin extends Plugin {
-  name = "MsaViewPlugin";
-  version = version;
+  name = 'MsaViewPlugin'
+  version = version
 
   install(pluginManager: PluginManager) {
-    pluginManager.addViewType(() => pluginManager.jbrequire(MsaViewFactory));
+    pluginManager.addViewType(
+      () =>
+        new ViewType({
+          name: 'MsaView',
+          //@ts-ignore
+          stateModel: MSAModel,
+          ReactComponent: MSAView,
+        }),
+    )
   }
 
   configure(pluginManager: PluginManager) {
     if (isAbstractMenuManager(pluginManager.rootModel)) {
-      pluginManager.rootModel.appendToSubMenu(["File", "Add"], {
-        label: "Multiple sequence alignment view",
-        icon: GridOn,
+      pluginManager.rootModel.appendToSubMenu(['File', 'Add'], {
+        label: 'Multiple sequence alignment view',
         onClick: (session: AbstractSessionModel) => {
-          session.addView("MsaView", {});
+          session.addView('MsaView', {})
         },
-      });
+      })
     }
   }
 }
