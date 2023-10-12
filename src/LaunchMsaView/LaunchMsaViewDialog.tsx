d06a7e43cdd7d7ae8e41423490da04fa2598f7a4
Colin <colin.diesh@gmail.com>
2023-10-12 13:29:12 -0400

    Fetch data

--- src/LaunchMsaView/LaunchMsaViewDialog.tsx
+++ src/LaunchMsaView/LaunchMsaViewDialog.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react'
-import { Dialog } from '@jbrowse/core/ui'
-import { Button, DialogActions, DialogContent } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Dialog, ErrorMessage } from '@jbrowse/core/ui'
+import {
+  Button,
+  DialogActions,
+  DialogContent,
+  MenuItem,
+  TextField,
+} from '@mui/material'
 import { Feature, getSession } from '@jbrowse/core/util'
 import { makeStyles } from 'tss-react/mui'
 
@@ -12,6 +18,19 @@ const useStyles = makeStyles()({
   },
 })
 
+export function getTranscriptFeatures(feature: Feature) {
+  // check if we are looking at a 'two-level' or 'three-level' feature by
+  // finding exon/CDS subfeatures. we want to select from transcript names
+  const subfeatures = feature.get('subfeatures') ?? []
+  return subfeatures.some(
+    f => f.get('type') === 'CDS' || f.get('type') === 'exon',
+  )
+    ? [feature]
+    : subfeatures
+}
+function getId(val?: Feature) {
+  return val !== undefined ? val.get('name') || val.get('id') : ''
+}
 export default function LaunchProteinViewDialog({
   handleClose,
   feature,
@@ -24,7 +43,38 @@ export default function LaunchProteinViewDialog({
 }) {
   const { classes } = useStyles()
   const session = getSession(model)
+  const [error, setError] = useState<unknown>()
+  const [data, setData] = useState<string[]>()
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    ;(async () => {
+      try {
+        const res = await fetch(
+          'https://jbrowse.org/demos/msaview/knownCanonical/list.txt',
+        )
+        if (!res.ok) {
+          throw new Error(
+            `HTTP ${res.status} fetching list ${await res.text()}`,
+          )
+        }
+        const result = await res.text()
+        setData(
+          result
+            .split('\n')
+            .map(f => f.trim())
+            .filter(f => !!f),
+        )
+      } catch (e) {
+        console.error(e)
+        setError(e)
+      }
+    })()
+  }, [])
+  const set = new Set(data)
+  const options = getTranscriptFeatures(feature)
 
+  const [userSelection, setUserSelection] = useState(getId(options[0]))
+  console.log({ options })
   return (
     <Dialog
       maxWidth="xl"
@@ -32,16 +82,50 @@ export default function LaunchProteinViewDialog({
       onClose={() => handleClose()}
       open
     >
-      <DialogContent className={classes.dialogContent}></DialogContent>
+      <DialogContent className={classes.dialogContent}>
+        {error ? <ErrorMessage error={error} /> : null}
+        <TextField
+          value={userSelection}
+          onChange={event => setUserSelection(event.target.value)}
+          label="Choose isoform"
+          select
+        >
+          {options.map((val, idx) => {
+            const d = getId(val)
+            return (
+              <MenuItem value={d} key={val.id() + '-' + idx}>
+                {d} {set.has(d) ? ' (has data)' : ''}
+              </MenuItem>
+            )
+          })}
+        </TextField>
+      </DialogContent>
       <DialogActions>
         <Button
           color="primary"
           variant="contained"
           onClick={() => {
-            session.addView('MsaView', {
-              type: 'MsaView',
-            })
-            handleClose()
+            ;(async () => {
+              try {
+                const res = await fetch(
+                  `https://jbrowse.org/demos/msaview/knownCanonical/${userSelection}.json`,
+                )
+                if (!res.ok) {
+                  throw new Error(
+                    `HTTP ${res.status} fetching ${await res.text()}`,
+                  )
+                }
+                const data = await res.json()
+                console.log({ data })
+                session.addView('MsaView', {
+                  type: 'MsaView',
+                })
+                handleClose()
+              } catch (e) {
+                console.error(e)
+                setError(e)
+              }
+            })()
           }}
         >
           Submit
