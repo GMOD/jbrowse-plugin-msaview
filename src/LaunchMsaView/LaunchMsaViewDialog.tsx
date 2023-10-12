5af76b44bc020d9811ba749d011c63eced1d0418
Colin <colin.diesh@gmail.com>
2023-10-12 14:38:27 -0400

    Hardcoded hierarchy

--- src/LaunchMsaView/LaunchMsaViewDialog.tsx
+++ src/LaunchMsaView/LaunchMsaViewDialog.tsx
@@ -119,9 +119,19 @@ export default function LaunchProteinViewDialog({
                 const d = new TextDecoder().decode(ungzip(data))
                 session.addView('MsaView', {
                   type: 'MsaView',
+                  treeAreaWidth: 200,
+                  treeWidth: 100,
+                  drawNodeBubbles: false,
+                  labelsAlignRight: true,
+                  showBranchLen: false,
+                  colWidth: 10,
+                  rowHeight: 12,
+                  colorSchemeName: 'percent_identity_dynamic',
+                  treeFilehandle: {
+                    uri: 'https://hgdownload.soe.ucsc.edu/goldenPath/hg38/multiz100way/hg38.100way.nh',
+                  },
                   data: {
                     msa: d,
-                    tree: '',
                   },
                 })
                 handleClose()
