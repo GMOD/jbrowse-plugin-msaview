f243cb51ca302a53ef0621c93c868b1f3c2115ad
Colin <colin.diesh@gmail.com>
2025-05-18 23:49:34 -0400

    Split watch script

--- esbuild-watch.mjs
+++ esbuild-watch.mjs
@@ -1,8 +1,11 @@
-import fs from 'fs'
 import * as esbuild from 'esbuild'
+import http from 'node:http'
 import { globalExternals } from '@fal-works/esbuild-plugin-global-externals'
 import JBrowseReExports from '@jbrowse/core/ReExports/list.js'
 import prettyBytes from 'pretty-bytes'
+
+const PORT = process.env.PORT ? +process.env.PORT : 9000
+const PORT2 = PORT + 400
 
 function createGlobalMap(jbrowseGlobals) {
   const globalMap = {}
@@ -15,18 +18,14 @@ function createGlobalMap(jbrowseGlobals) {
   return globalMap
 }
 
-const result = await esbuild.build({
+let ctx = await esbuild.context({
   entryPoints: ['src/index.ts'],
   bundle: true,
   globalName: 'JBrowsePluginMsaView',
-  sourcemap: true,
-  outfile: 'dist/jbrowse-plugin-msaview.umd.production.min.js',
+  outfile: 'dist/out.js',
   metafile: true,
-  minify: true,
   plugins: [
-    globalExternals(
-      createGlobalMap([...JBrowseReExports.default, '@jbrowse/core/ui/theme']),
-    ),
+    globalExternals(createGlobalMap(JBrowseReExports.default)),
     {
       name: 'rebuild-log',
       setup({ onStart, onEnd }) {
@@ -53,5 +52,39 @@ const result = await esbuild.build({
     },
   ],
 })
+let { hosts } = await ctx.serve({
+  servedir: '.',
+  port: PORT2,
+})
 
-fs.writeFileSync('meta.json', JSON.stringify(result.metafile))
+http
+  .createServer((req, res) => {
+    const proxyReq = http.request(
+      {
+        hostname: hosts[0],
+        port: PORT2,
+        path: req.url,
+        method: req.method,
+        headers: req.headers,
+      },
+      proxyRes => {
+        //restore the CORS after
+        //https://github.com/evanw/esbuild/releases/tag/v0.25.0 disabled it
+        //as a potential vuln
+        res.writeHead(proxyRes.statusCode, {
+          ...proxyRes.headers,
+          'Access-Control-Allow-Origin': '*',
+        })
+        proxyRes.pipe(res, { end: true })
+      },
+    )
+
+    // Forward the body of the request to esbuild
+    req.pipe(proxyReq, { end: true })
+  })
+  .listen(PORT)
+
+console.log(`Serving at http://${hosts[0]}:${PORT}`)
+
+await ctx.watch()
+console.log('Watching files...')
