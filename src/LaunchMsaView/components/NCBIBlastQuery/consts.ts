a189554c752ac1721daa31f5c21b426ce64feed5
Colin <colin.diesh@gmail.com>
2026-04-13 19:03:35 -0400

    Pnpm bump

--- src/LaunchMsaView/components/NCBIBlastQuery/consts.ts
+++ src/LaunchMsaView/components/NCBIBlastQuery/consts.ts
@@ -1,1 +1,3 @@
 export const BASE_BLAST_URL = 'https://blast.ncbi.nlm.nih.gov/Blast.cgi'
+export const msaAlgorithms = ['clustalo', 'muscle', 'kalign', 'mafft'] as const
+export type MsaAlgorithm = (typeof msaAlgorithms)[number]
